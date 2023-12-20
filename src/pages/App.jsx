d74c9795d0851deed62e003cd404f9acc537e6d7
Brandon Cousins <brandon509@gmail.com>
2023-12-19 22:00:38 -0500

    fixed bugs

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -49,7 +49,7 @@ export default function App () {
           <div className='main'>
             <div>
               <p className='label'>Meat weight:</p>
-              <input className='inputWeight' onChange={onChangeWeight} value={weight>0 && weight} type='number' inputmode="numeric" pattern="[0-9]*"></input>
+              <input className='inputWeight' onChange={onChangeWeight} value={weight>0 && weight} type='number'></input>
               <p className='lbsLabel'>lbs</p>
             </div>
             <table className='amountTable'>
