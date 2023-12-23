d6ea5675237fc8d00b46f08a2ff05d6f6a9e6817
Brandon Cousins <brandon509@gmail.com>
2023-12-22 23:40:58 -0500

    cleaned up css and class names

--- src/components/percentage-input.jsx
+++ src/components/percentage-input.jsx
@@ -1,8 +1,8 @@
 export default function PercentageInput ({ name, value, onChange, onClickUp, onClickDown}) {
 
     return(
-        <div className="inputParent">
-        <label className="inputLabel">{name}</label><button className='minus' id={name} onClick={onClickDown}>-</button><input value={value} onChange={onChange} className='ingredientInput' name={name} type='number'></input><button className='plus' id={name} onClick={onClickUp}>+</button><p className="percent">%</p>
+        <div className="input-top">
+        <label className="input-label">{name}</label><button className='minus' id={name} onClick={onClickDown}>-</button><input value={value} onChange={onChange} className='ingredient-input' name={name} type='number'></input><button className='plus' id={name} onClick={onClickUp}>+</button><p className="percent">%</p>
         </div>
     )
 }
