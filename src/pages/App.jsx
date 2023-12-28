4ea86768613d08c78037bab95a1aa8750f1419a3
Brandon Cousins <brandon509@gmail.com>
2023-12-28 17:21:21 -0500

    bug fixes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -49,7 +49,7 @@ export default function App () {
           <div className='main'>
             <div className='weight-input-section'>
               <label for='weight-input' className='weight-label'>Weight</label>
-              <input id='weight-input' onChange={onChangeWeight} value={weight>0 && weight} type='number'></input>
+              <input id='weight-input' onChange={onChangeWeight} value={weight>0 && weight} type='number' inputMode='decimal'></input>
               {/* <p className='lbs'>lbs</p> */}
             </div>
             <table>
