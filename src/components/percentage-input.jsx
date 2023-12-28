2abc46e7d811de3b13076dd3c70af0d09d34e7f7
Brandon Cousins <brandon509@gmail.com>
2023-12-28 16:29:36 -0500

    big fix

--- src/components/percentage-input.jsx
+++ src/components/percentage-input.jsx
@@ -4,8 +4,7 @@ export default function PercentageInput ({ name, value, onChange, onClickUp, onC
         <div className="input-top">
             <label className="input-label">{name}</label>
             <button className='minus' id={name} onClick={onClickDown}>-</button>
-            <input value={value} onChange={onChange} className='ingredient-input' name={name} type="number" inputMode='decimal' 
-   pattern='\d*'></input>
+            <input value={value} onChange={onChange} className='ingredient-input' name={name} type="number" inputMode='decimal'></input>
             <button className='plus' id={name} onClick={onClickUp}>+</button>
             <p className="percent">%</p>
         </div>
